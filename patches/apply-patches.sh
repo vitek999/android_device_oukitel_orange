f9a6f893d244386d71dcd13d896e5bcae610bed7
vitek999 <vitek2012rus@gmail.com>
2017-01-08 12:12:28 +0300

    Revert "changes for last cm updates - fix kernel panic on start"
    This reverts commit 054a0b82d644ce6f40360f494d31e60a9a7dc1cc.

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,13 +4,8 @@ cd frameworks/av
 git apply -v ../../device/oukitel/orange/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch
 cd ../..
 cd system/core
-<<<<<<< HEAD
 git apply -v ../../device/oukitel/orange/patches/system_core/0001-remove-CAP_SYS_NICE-from-surfaceflinger-and-cln-logg.patch
 #git apply -v ../../device/oukitel/orange/patches/system_core/0002-Changes-for-more-level-log.patch
-=======
-git apply -v ../../device/archos/persimmon/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-#git apply -v ../../device/archos/persimmon/patches/system_core/0002-Changes-for-more-level-log.patch
->>>>>>> ce0b023... changes for last cm updates - fix kernel panic on start
 cd ../..
 cd system/netd
 git apply -v ../../device/oukitel/orange/patches/system_netd/0001-Enable-Tethering.patch
